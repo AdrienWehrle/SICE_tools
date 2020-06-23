4f7bc3e5ac41bccef0f138aeb533bd76cb4761f4
Adrien Wehrlé <44843368+AdrienWehrle@users.noreply.github.com>
2020-06-23 17:45:41 +0200

    Update SICE_processing.py

--- SICE_processing.py
+++ SICE_processing.py
@@ -189,7 +189,7 @@ if __name__ == '__main__':
     #and daily cumulative filtered BBAs as values
     filtered_BBAs_cumulative={}
     
-    #gapfilling forwards
+    #gap-filling forwards
     for i,key in enumerate(filtered_BBAs):
         
         if i==0:
