6376109e8c016f54e1d81e2274a42ffe57ee83f8
Adrien Wehrle <adrien.wehrle@hotmail.fr>
2020-11-02 10:18:38 +0100

    Syntax in line with the SICE toolchain

--- SCDA.py
+++ SCDA.py
@@ -37,27 +37,28 @@ OUTPUTS:
 
 import numpy as np
 from numpy import asarray as ar
-import rasterio 
+import rasterio
 import argparse
 import os
 import time
 import multiprocessing
 from multiprocessing import Pool
 
-multi_proc=False
+multi_proc = False
 
 parser = argparse.ArgumentParser()
 parser.add_argument('inpath')
 args = parser.parse_args()
 
 
-def radiometric_calibration(R16,scene,inpath=args.inpath):
+def radiometric_calibration(R16, scene, inpath):
     '''
     Sentinel-3 Product Notice – SLSTR:
-    "Based on the analysis performed to-date, a recommendation has been put forward to users to
-    adjust the S5 and S6 reflectances by factors of 1.12 and 1.20 respectively in the nadir view and
-    1.15 and 1.26 in the oblique view. Uncertainty estimates on these differences are still to be
-    evaluated and comparisons with other techniques have yet to be included."
+    "Based on the analysis performed to-date, a recommendation has been put
+    forward to users to adjust the S5 and S6 reflectances by factors of 1.12
+    and 1.20 respectively in the nadir view and 1.15 and 1.26 in the oblique
+    view. Uncertainty estimates on these differences are still to be evaluated 
+    and comparisons with other techniques have yet to be included."
     
     INPUTS:
         R16: Dataset reader for Top of Atmosphere (TOA) reflectance channel S5.
@@ -69,18 +70,17 @@ def radiometric_calibration(R16,scene,inpath=args.inpath):
                                   reflectance for channel S5.
     '''
     
-    profile_R16=R16.profile
-    factor=1.12
-    R16_data=R16.read(1)
-    R16_rc=R16_data*factor
+    profile_R16 = R16.profile
+    factor = 1.12
+    R16_data = R16.read(1)
+    R16_rc = R16_data * factor
     
-    with rasterio.open(inpath+os.sep+scene+os.sep+'r_TOA_S5_rc.tif','w',**profile_R16) as dst:
+    with rasterio.open(inpath + 'r_TOA_S5_rc.tif', 'w', **profile_R16) as dst:
         dst.write(R16_rc, 1)
     
-    
-    
-    
-def SCDA_v20(R550,R16,BT37,BT11,BT12,profile,scene,inpath=args.inpath, SICE_toolchain=True):
+       
+def SCDA_v20(R550, R16, BT37, BT11, BT12, profile, scene, inpath, 
+             SICE_toolchain=True):
     '''
     
     INPUTS:
@@ -105,61 +105,61 @@ def SCDA_v20(R550,R16,BT37,BT11,BT12,profile,scene,inpath=args.inpath, SICE_tool
          
     '''
     
-    #determining the NDSI, needed for the cloud detection
-    NDSI=(R550-R16)/(R550+R16)
-    with rasterio.open(inpath+os.sep+scene+os.sep+'NDSI.tif','w',**profile) as dst:
+    # determining the NDSI, needed for the cloud detection
+    NDSI = (R550 - R16) / (R550 + R16)
+    
+    with rasterio.open(path + 'NDSI.tif', 'w', **profile) as dst:
         dst.write(NDSI, 1)
     
-    #initializing thresholds
-    base=np.empty((R550.shape[0],R550.shape[1]))
-    THR=base.copy()
-    THR[:]=np.nan
-    THRmax=base.copy()
-    THRmax[:]=-5.5 
-    S=base.copy()
-    S[:]=1.1
+    # initializing thresholds
+    base = np.empty((R550.shape[0], R550.shape[1]))
+    THR = base.copy()
+    THR[:] = np.nan
+    THRmax = base.copy()
+    THRmax[:] = -5.5
+    S = base.copy()
+    S[:] = 1.1
     
-    #tests 1 to 5, only based on inputs
-    t1=ar(R550>0.30)*ar(NDSI/R550<0.8)*ar(BT12<=290)
-    t2=ar(BT11-BT37<-13)*ar(R550>0.15)*ar(NDSI >= -0.30)\
-       *ar(R16>0.10)*ar(BT12<=293)
-    t3=ar(BT11-BT37<-30)
-    t4=ar(R550<0.75)*ar(BT12>265)
-    t5=ar(R550>0.75)
+    # tests 1 to 5, only based on inputs
+    t1 = ar(R550 > 0.30) * ar(NDSI / R550 < 0.8) * ar(BT12 <= 290)
+    t2 = ar(BT11 - BT37 < -13) * ar(R550 > 0.15) * ar(NDSI >= -0.30)\
+        * ar(R16 > 0.10) * ar(BT12 <= 293)
+    t3 = ar(BT11 - BT37 < -30)
+    t4 = ar(R550 < 0.75) * ar(BT12 > 265)
+    t5 = ar(R550 > 0.75)
     
+    cloud_detection = t1
+    cloud_detection[cloud_detection is False] = t2[cloud_detection is False]
+    cloud_detection[cloud_detection is False] = t3[cloud_detection is False]
     
-    cloud_detection=t1
-    cloud_detection[cloud_detection==False]=t2[cloud_detection==False]
-    cloud_detection[cloud_detection==False]=t3[cloud_detection==False]
+    THR1 = 0.5 * BT12 - 133
     
-    THR1=0.5*BT12-133
+    THRmax[t4 is False] = -8
+    THR = np.minimum(THR1, THRmax)
+    S[t5 is False] = 1.5
     
-    THRmax[t4==False]=-8
-    THR=np.minimum(THR1,THRmax)
-    S[t5==False]=1.5
-    
-    #test 6, based on fluctuating thresholds
-    t6=ar(BT11-BT37<THR)*ar(NDSI/R550<S)*ar((NDSI>=-0.02) & (NDSI<=0.75))\
-       *ar(BT12<=270)*ar(R550>0.18)
+    # test 6, based on fluctuating thresholds
+    t6 = ar(BT11 - BT37 < THR) * ar(NDSI / R550 < S)\
+        * ar((NDSI >= -0.02) & (NDSI <= 0.75)) * ar(BT12 <= 270) * ar(R550 > 0.18)
 
-    cloud_detection[cloud_detection==False]=t6[cloud_detection==False]
+    cloud_detection[cloud_detection is False] = t6[cloud_detection is False]
     
     if SICE_toolchain:
-        cloud_detection[cloud_detection==True]=255
-        cloud_detection[cloud_detection==False]=1
+        cloud_detection[cloud_detection is True] = 255
+        cloud_detection[cloud_detection is False] = 1
     
-    #writing results
-    profile_cloud_detection=profile.copy()
+    # writing results
+    profile_cloud_detection = profile.copy()
     profile_cloud_detection.update(dtype=rasterio.int16)
-    with rasterio.open(inpath+os.sep+scene+os.sep+'SCDA_v20.tif','w',**profile_cloud_detection) as dst:
+    
+    with rasterio.open(path + 'SCDA_v20.tif', 'w', **profile_cloud_detection) as dst:
         dst.write(cloud_detection.astype(np.int16), 1)
     
     return cloud_detection, NDSI
 
 
-
-
-def SCDA_v14(R550,R16,BT37,BT11,BT12,NDSI,profile,scene,inpath=args.inpath,NDSI_cp=False, SICE_toolchain=True):
+def SCDA_v14(R550, R16, BT37, BT11, BT12, NDSI, profile, scene, path, 
+             NDSI_cp=False, SICE_toolchain=True):
     '''
     
     INPUTS:
@@ -187,101 +187,111 @@ def SCDA_v14(R550,R16,BT37,BT11,BT12,NDSI,profile,scene,inpath=args.inpath,NDSI_
          
     '''
     
-    if NDSI_cp==True:
-        #determining the NDSI, needed for the cloud detection
-        NDSI=(R550-R16)/(R550+R16)
-        with rasterio.open(inpath+os.sep+scene+os.sep+'NDSI.tif','w',**profile) as dst:
+    if NDSI_cp:
+        
+        # determining the NDSI, needed for the cloud detection
+        NDSI = (R550 - R16) / (R550 + R16)
+        with rasterio.open(path + 'NDSI.tif', 'w', **profile) as dst:
             dst.write(NDSI, 1)
         
-    diff=BT11-BT37
-    diff_threshold=0.5*BT12-131
+    diff = BT11 - BT37
+    diff_threshold = 0.5 * BT12 - 131
     
-    diff_threshold[diff_threshold>-6]=-6
+    diff_threshold[diff_threshold > -6] = -6
     
-    #tests 1 and 2, only based on inputs
-    t1=ar(diff<=diff_threshold)*ar(BT12<287)*ar((NDSI>-0.2) & (NDSI<0.69))\
-       *ar(R550>20)
-    t2=ar(diff<-3)*ar(diff>diff_threshold)*ar(BT12<287)*ar((NDSI>-0.05) & (NDSI<0.6))\
-       *ar(R550>20)*ar(NDSI*100<1.1*R550)
+    # tests 1 and 2, only based on inputs
+    t1 = ar(diff <= diff_threshold) * ar(BT12 < 287) *\
+        ar((NDSI > -0.2) & (NDSI < 0.69)) * ar(R550 > 20)
+    t2 = ar(diff < -3) * ar(diff > diff_threshold) * ar(BT12 < 287)\
+        * ar((NDSI > -0.05) & (NDSI < 0.6)) * ar(R550 > 20)\
+        * ar(NDSI * 100 < 1.1 * R550)
        
-    cloud_detection=t1
-    cloud_detection[cloud_detection==False]=t2[cloud_detection==False]
+    cloud_detection = t1
+    cloud_detection[cloud_detection is False] = t2[cloud_detection is False]
     
     if SICE_toolchain:
-        cloud_detection[cloud_detection==True]=255
-        cloud_detection[cloud_detection==False]=1
+        cloud_detection[cloud_detection is True] = 255
+        cloud_detection[cloud_detection is False] = 1
     
-    #writing results
-    profile_cloud_detection=profile.copy()
+    # writing results
+    profile_cloud_detection = profile.copy()
     profile_cloud_detection.update(dtype=rasterio.int16)
-    with rasterio.open(inpath+os.sep+scene+os.sep+'SCDA_v14.tif','w',**profile_cloud_detection) as dst:
-        dst.write(cloud_detection.astype(np.int16), 1)
     
+    with rasterio.open(path + 'SCDA_v14.tif', 'w', **profile_cloud_detection) as dst:
+        dst.write(cloud_detection.astype(np.int16), 1)
     
     return cloud_detection
 
 
+# listing scenes for a given date
+scenes = os.listdir(args.inpath)
 
-
-
-#listing scenes for a given date
-scenes=os.listdir(args.inpath)
-
-if multi_proc==False:
-    for i,scene in enumerate(scenes):
+if not multi_proc:
+    
+    for i, scene in enumerate(scenes):
         
-        #saving profile metadata only for the first iteration
-        profile=rasterio.open(args.inpath+os.sep+scene+os.sep+'r_TOA_S1.tif').profile
+        # creating scene specific path
+        path = args.inpath + os.sep + scene + os.sep
         
-        #calibrating R16
-        R16=rasterio.open(args.inpath+os.sep+scene+os.sep+'r_TOA_S5.tif')
-        radiometric_calibration(R16=R16,scene=scene)
+        # saving profile metadata only for the first iteration
+        profile = rasterio.open(path + 'r_TOA_S1.tif').profile
         
-        #loading inputs
-        R550=rasterio.open(args.inpath+os.sep+scene+os.sep+'r_TOA_S1.tif').read(1)
-        R16=rasterio.open(args.inpath+os.sep+scene+os.sep+'r_TOA_S5_rc.tif').read(1)
-        BT37=rasterio.open(args.inpath+os.sep+scene+os.sep+'BT_S7.tif').read(1)
-        BT11=rasterio.open(args.inpath+os.sep+scene+os.sep+'BT_S8.tif').read(1)
-        BT12=rasterio.open(args.inpath+os.sep+scene+os.sep+'BT_S9.tif').read(1)
+        # calibrating R16
+        R16 = rasterio.open(path + 'r_TOA_S5.tif')
+        radiometric_calibration(R16=R16, scene=scene, inpath=path)
         
-        #running SCDA v2.0 and v1.4
-        cd,NDSI=SCDA_v20(R550=R550,R16=R16,BT37=BT37,BT11=BT11,BT12=BT12,scene=scene,profile=profile)
-        SCDA_v14(R550=R550,R16=R16,BT37=BT37,BT11=BT11,BT12=BT12,NDSI=NDSI,scene=scene,profile=profile)
+        # loading inputs
+        R550 = rasterio.open(path + 'r_TOA_S1.tif').read(1)
+        R16 = rasterio.open(path + 'r_TOA_S5_rc.tif').read(1)
+        BT37 = rasterio.open(path + 'BT_S7.tif').read(1)
+        BT11 = rasterio.open(path + 'BT_S8.tif').read(1)
+        BT12 = rasterio.open(path + 'BT_S9.tif').read(1)
+        
+        # running SCDA v2.0 and v1.4
+        cd, NDSI = SCDA_v20(R550=R550, R16=R16, BT37=BT37, BT11=BT11, BT12=BT12,
+                            scene=scene, profile=profile, inpath=path)
+        
+        SCDA_v14(R550=R550, R16=R16, BT37=BT37, BT11=BT11, BT12=BT12, NDSI=NDSI,
+                 scene=scene, profile=profile, inpath=path)
         
         print('%s: done' % scene)
         
-      
-        
-if multi_proc==True:
+            
+if multi_proc:
     
     def multiproc_SCDA(k):
-        #saving profile metadata only for the first iteration
-        if k==0:
-            profile=rasterio.open(args.inpath+os.sep+scenes[k]+os.sep+'r_TOA_S1.tif').profile
         
-        #calibrating R16
-        R16=rasterio.open(args.inpath+os.sep+scenes[k]+os.sep+'r_TOA_S5.tif')
-        radiometric_calibration(R16=R16,scene=scenes[k])
+        # creating scene specific path
+        path = args.inpath + os.sep + scene + os.sep
         
-        #loading inputs
-        R550=rasterio.open(args.inpath+os.sep+scenes[k]+os.sep+'r_TOA_S1.tif').read(1)
-        R16=rasterio.open(args.inpath+os.sep+scenes[k]+os.sep+'r_TOA_S5_rc.tif').read(1)
-        BT37=rasterio.open(args.inpath+os.sep+scenes[k]+os.sep+'BT_S7.tif').read(1)
-        BT11=rasterio.open(args.inpath+os.sep+scenes[k]+os.sep+'BT_S8.tif').read(1)
-        BT12=rasterio.open(args.inpath+os.sep+scenes[k]+os.sep+'BT_S9.tif').read(1)
+        # saving profile metadata only for the first iteration
+        if k == 0:
+            profile = rasterio.open(path + 'r_TOA_S1.tif').profile
         
-        #running SCDA v2.0 and v1.4
-        cd,NDSI=SCDA_v20(R550=R550,R16=R16,BT37=BT37,BT11=BT11,BT12=BT12,scene=scenes[k],profile=profile)
-        SCDA_v14(R550=R550,R16=R16,BT37=BT37,BT11=BT11,BT12=BT12,NDSI=NDSI,scene=scenes[k],profile=profile)
+        # calibrating R16
+        R16 = rasterio.open(path + 'r_TOA_S5.tif')
+        radiometric_calibration(R16=R16, scene=scenes[k], inpath=path)
         
-
-    #multiprocessing run    
-    nb_cores=multiprocessing.cpu_count()
+        # loading inputs
+        R550 = rasterio.open(path + 'r_TOA_S1.tif').read(1)
+        R16 = rasterio.open(path + 'r_TOA_S5_rc.tif').read(1)
+        BT37 = rasterio.open(path + 'BT_S7.tif').read(1)
+        BT11 = rasterio.open(path + 'BT_S8.tif').read(1)
+        BT12 = rasterio.open(path + 'BT_S9.tif').read(1)
+        
+        # running SCDA v2.0 and v1.4
+        cd, NDSI = SCDA_v20(R550=R550, R16=R16, BT37=BT37, BT11=BT11, BT12=BT12,
+                            scene=scenes[k], profile=profile, inpath=path)
+        
+        SCDA_v14(R550=R550, R16=R16, BT37=BT37, BT11=BT11, BT12=BT12, NDSI=NDSI,
+                 scene=scenes[k], profile=profile, inpath=path)
+        
+    # multiprocessing run
+    nb_cores = multiprocessing.cpu_count()
     start_time = time.time()
     
     if __name__ == '__main__':
         with Pool(nb_cores) as p:
-            p.map(multiproc_SCDA, list(range(0,len(scenes))))
+            p.map(multiproc_SCDA, range(0, len(scenes)))
 
     print("--- %s seconds ---" % (time.time() - start_time))
-    
